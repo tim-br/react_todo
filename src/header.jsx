b840f0cfb9026505fbe5ac3a554e1cf86deca873
tmtwd <t.williams.im@gmail.com>
2015-09-20 13:15:04 -0400

    first commit

---
+++ src/header.jsx
@@ -0,0 +1,35 @@
+var React = require('react');
+
+module.exports = React.createClass({
+	getInitialState: function(){
+		return{
+			text: ''
+		}
+	},
+	render: function(){
+		return <div className="input-group">
+			<input 
+				value={this.state.text}
+				onChange={this.handleInputChange}
+				type="text" 
+				className="form-control" />
+			<span className="input-group-btn">
+				<button 
+					onClick={this.handleClick}
+					className='btn btn-default' 
+					type='button'>
+					Add
+				</button>
+			</span>
+			{this.state.text}
+		</div>
+	},
+
+	handleClick: function(){
+		//send value of text input to firebase
+		console.log(this.state.text)
+	},
+	handleInputChange: function(event){
+		this.setState({text: event.target.value});
+	}
+})
